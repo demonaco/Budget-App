f13ba87304d7ef3c6980b05d493702cf61761794
Matt DeMonaco <mdemonaco@gmail.com>
2020-01-15 21:49:41 -0800

    Added Bootstrap CDN, linked CSS, and implemented Jumbotron.

---
+++ app.js
@@ -0,0 +1,38 @@
+var budgetController = (function() {
+    var x = 23;
+
+   
+})();
+
+var UIController = (function() {
+
+})();
+
+
+//global app controller
+
+var controller = (function() {
+
+    var ctrlAddItem = function(){
+
+
+        
+    }
+
+    document.querySelector('.add__button').addEventListener("click", function() {
+        
+  
+  
+  
+    });
+
+    document.addEventListener('keypress', function(event){
+        
+        if(event.keycode === 13 || event.which === 13) {
+
+        }
+
+    })
+
+
+})(budgetController, UIController);
